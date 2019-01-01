c1a95d66a8cf5d00cb0152e9e5f7dc1af2d88f1e
caioxcezar <caioxcezar@gmail.com>
2018-12-31 23:07:40 -0200

    Add wikipedia search

--- Index.js
+++ Index.js
@@ -1,14 +1,22 @@
 let form = document.getElementById("search-form")
 let select = document.getElementById("search-engine")
 let img = document.getElementById("search-img")
+let input = document.getElementById("search-input")
 select.onchange = () => {
     form.action = select.value
     switch (select.selectedIndex) {
         case 0:
-        img.style.backgroundImage = "url('pics/DuckDuckGo.svg')"
+            img.style.backgroundImage = "url('pics/DuckDuckGo.svg')"
+            img.style.backgroundSize = "auto"
             break
         case 1:
             img.style.backgroundImage = "url('pics/Google.svg')"
+            img.style.backgroundSize = "80%"
+            break
+        case 2:
+            img.style.backgroundImage = "url('pics/Wikipedia.svg')"
+            input.name = "search"
+            img.style.backgroundSize = "90%"
             break
     }
     
